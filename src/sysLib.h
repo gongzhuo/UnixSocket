bfe765c8de63e7f7032dac3f39e55b01289cb1b9
yuhao10 <yuhao10@huawei.com>
2017-10-27 15:34:05 +0800

    增加linux系统依赖

---
+++ src/sysLib.h
@@ -0,0 +1,8 @@
+#ifndef	sysLib
+#define	sysLib
+
+#include	<sys/types.h>	/* basic system data types */
+#include	<sys/socket.h>	/* basic socket definitions */
+#include	<sys/time.h>	/* timeval{} for select() */
+
+#endif
